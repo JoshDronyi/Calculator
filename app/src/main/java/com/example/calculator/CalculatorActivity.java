70aed81a9004d2d5a0a11b9a043bf395c84dad7d
Josh <joshuadronyi@gmail.com>
2019-09-18 13:23:53 -0400

    Calculator not working. Numbers work and will go to screen. Operators appear on screen. Onli addition operator is ready for me to code the equals button.

--- app/src/main/java/com/example/calculator/CalculatorActivity.java
+++ app/src/main/java/com/example/calculator/CalculatorActivity.java
@@ -1,6 +1,8 @@
 package com.example.calculator;
 
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,20 +15,136 @@ public class CalculatorActivity extends AppCompatActivity {
 
     //Variables for the calculator
 
-    TextView display;
-    Button btnZero, btnOne, btnTwo, btnThree,btnFour,btnFive,btnSix,btnSeven,btnEight,btnNine;
+    TextView screen;
+    Button btnZero, btnOne, btnTwo, btnThree, btnFour, btnFive, btnSix, btnSeven, btnEight, btnNine;
     Button btnPoint, btnClear, btnPlus, btnMinus, btnMultiply, btnDivide, btnEquals;
+
+    Double firstNumber = null, secondNumber = null;
+    String operation;
+
+
+    public final static String DEFAULT_SCREEN_VALUE = "0";
+    public final static String ADDITION = "+";
+    public final static String SUBTRACTION = "-";
+    public final static String MULTIPLICATION = "x";
+    public final static String DIVISION = "/";
+    public final static String CLEAR = "C";
 
     View.OnClickListener listener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
 
             Button buttonInstance = (Button) view;
-            Toast.makeText(CalculatorActivity.this, buttonInstance.getText() , Toast.LENGTH_SHORT).show();
+            String newText = null;
+
+            screen = findViewById(R.id.screen);
+
+            String screenText = screen.getText().toString();
+            String buttonText = buttonInstance.getText().toString();
+
+            if (screenText.startsWith(DEFAULT_SCREEN_VALUE)) {
+                newText = buttonInstance.getText().toString();
+                Toast.makeText(CalculatorActivity.this, "Default Values", Toast.LENGTH_SHORT).show();
+            } else if (buttonText.equals(ADDITION)) {
+                Toast.makeText(CalculatorActivity.this, "Adding new Values", Toast.LENGTH_SHORT).show();
+
+                Log.i("Adding", "ADDING NEW VALUES: ******CURRENT VALUE FOR********** VAR SCREENTEXT = " + screenText);
+
+                newText = Add(screenText);
+
+            } else if (buttonText.equals((SUBTRACTION))) {
+                Toast.makeText(CalculatorActivity.this, "Subtractiong Values", Toast.LENGTH_SHORT).show();
+
+                newText = Subtract(screenText);
+            } else if (buttonText.equals((MULTIPLICATION))) {
+                Toast.makeText(CalculatorActivity.this, "Multiplying Values", Toast.LENGTH_SHORT).show();
+
+                newText = Multiply(screenText);
+            } else if (buttonText.equals((DIVISION))) {
+                Toast.makeText(CalculatorActivity.this, "Dividing Values", Toast.LENGTH_SHORT).show();
+
+                newText = Divide(screenText);
+            } else if (buttonText.equalsIgnoreCase("C")) {
+                screen.setText("");
+                firstNumber = null;
+                secondNumber = null;
+                operation = null;
+            } else {
+                newText = screen.getText().toString() + buttonInstance.getText();
+            }
+
+            screen.setText(newText);
+
+            Toast.makeText(CalculatorActivity.this, buttonInstance.getText(), Toast.LENGTH_SHORT).show();
 
         }
     };
 
+    private String Divide(String currentScreenText) {
+        String newScreenText;
+        int operatorIndex;
+
+        if (currentScreenText.contains("/")) {
+            operatorIndex = currentScreenText.indexOf("/");
+
+            firstNumber = Double.parseDouble(currentScreenText.substring(0, operatorIndex));
+            Toast.makeText(this, "" + firstNumber, Toast.LENGTH_SHORT).show();
+
+        }
+        newScreenText = firstNumber + " / ";
+
+        return newScreenText;
+    }
+
+    private String Multiply(String currentScreenText) {
+        String newScreenText;
+        int operatorIndex;
+
+        if (currentScreenText.contains("x")) {
+            operatorIndex = currentScreenText.indexOf("x");
+
+            firstNumber = Double.parseDouble(currentScreenText.substring(0, operatorIndex));
+            Toast.makeText(this, "" + firstNumber, Toast.LENGTH_SHORT).show();
+
+        }
+        newScreenText = firstNumber + " x ";
+
+        return newScreenText;
+    }
+
+    private String Subtract(String currentScreenText) {
+        String newScreenText;
+        int operatorIndex;
+
+        if (currentScreenText.contains("-")) {
+            operatorIndex = currentScreenText.indexOf("-");
+
+            firstNumber = Double.parseDouble(currentScreenText.substring(0, operatorIndex));
+            Toast.makeText(this, "" + firstNumber, Toast.LENGTH_SHORT).show();
+
+        }
+        newScreenText = firstNumber + " - ";
+
+        return newScreenText;
+    }
+
+    public String Add(String currentScreenText) {
+        String newScreenText;
+        int operatorIndex;
+
+        if (currentScreenText.contains("+")) {
+            operatorIndex = currentScreenText.indexOf("+");
+
+            firstNumber = Double.parseDouble(currentScreenText.substring(0, operatorIndex));  //Because we do not want the operator in this
+            Toast.makeText(this, " " + firstNumber, Toast.LENGTH_SHORT).show();
+        } else {
+            firstNumber = Double.parseDouble(currentScreenText);
+        }
+
+        newScreenText = firstNumber + " + ";
+
+        return newScreenText;
+    }
 
 
     @Override
@@ -74,6 +192,7 @@ public class CalculatorActivity extends AppCompatActivity {
         btnDivide.setOnClickListener(listener);
         btnClear.setOnClickListener(listener);
         btnEquals.setOnClickListener(listener);
+
 
     }
 }
